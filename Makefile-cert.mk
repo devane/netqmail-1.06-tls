971a407b8f14096fee8fe295724b6a0cb8391c08
Nikolay Hristov <geroy@horizon9.org>
2017-07-20 14:05:38 +0300

    make cert fix

--- Makefile-cert.mk
+++ Makefile-cert.mk
@@ -1,11 +1,11 @@
 cert-req: req.pem
-cert cert-req: QMAIL/control/clientcert.pem
+cert cert-req: clientcert.pem
 	@:
 
-QMAIL/control/clientcert.pem: QMAIL/control/servercert.pem
+clientcert.pem: servercert.pem
 	ln -s $< $@
 
-QMAIL/control/servercert.pem:
+servercert.pem:
 	PATH=$$PATH:/usr/local/ssl/bin \
 		openssl req -new -x509 -nodes -days 366 -out $@ -keyout $@
 	chmod 640 $@
@@ -13,9 +13,9 @@ QMAIL/control/servercert.pem:
 
 req.pem:
 	PATH=$$PATH:/usr/local/ssl/bin openssl req \
-		-new -nodes -out $@ -keyout QMAIL/control/servercert.pem
+		-new -nodes -out $@ -keyout control/servercert.pem
 	chmod 640 QMAIL/control/servercert.pem
-	chown `head -2 conf-users | tail -1`:`head -1 conf-groups` QMAIL/control/servercert.pem
+	chown `head -2 conf-users | tail -1`:`head -1 conf-groups` servercert.pem
 	@echo
 	@echo "Send req.pem to your CA to obtain signed_req.pem, and do:"
 	@echo "cat signed_req.pem >> QMAIL/control/servercert.pem"
